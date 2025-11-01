da4ccbb4d131685b16530c3f83fc73a7b8a52752
37signalsS <misha-eliseyev@mail.ru>
2025-11-01 22:38:35 +0300

    add

--- waybar/scripts/cpu.sh
+++ waybar/scripts/cpu.sh
@@ -2,7 +2,7 @@
 
 cpu_temp=$(sensors k10temp-pci-00c3 | awk '/^Tctl:/ {print $2}' | tr -d '+°C' | cut -d. -f1)
 
-ram_temp=$(sensors spd5118-i2c-2-51 | awk '/^temp1:/ {print $2}' | tr -d '+°C' | cut -d. -f1)
+ram_temp=$(sensors spd5118-i2c-2-50 | awk '/^temp1:/ {print $2}' | tr -d '+°C' | cut -d. -f1)
 
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{printf "%.0f%%", 100 - $8}')
 
